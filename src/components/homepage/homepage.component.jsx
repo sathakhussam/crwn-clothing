a74a51f52dd3d562380101f2d5c320868afdae0c
Sathak Uzham <sathakhussam@gmail.com>
2021-04-13 00:26:28 +0530

    Make Homepage component and layout and content for the component

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const Homepage = props => (
+    <div className="homepage">
+        <div className="directory-menu">
+            {/* Every Single Directory  */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            {/* Every Single Directory  */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            {/* Every Single Directory  */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            {/* Every Single Directory  */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            {/* Every Single Directory  */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+
+
+export default Homepage;
