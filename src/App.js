295528999089ea3ecac07a4aa3cda93bed8b1534
Sathak Uzham <sathakhussam@gmail.com>
2021-04-13 01:17:14 +0530

    Make images and make seperate component (Directory and MenuItem) from the homepage component and move Homepage component from component folder to pages folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Homepage from './components/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component'
 import './App.css';
 
 function App() {
