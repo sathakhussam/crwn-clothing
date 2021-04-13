412d252921961a014367d2a2df6a44daab1fd618
Sathak Uzham <sathakhussam@gmail.com>
2021-04-13 12:57:16 +0530

    Add react router to the project and make the route for hats page in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Homepage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
 import './App.css';
+
+const HatsPage = props => {
+  return <div>
+    <h1>Hats Page</h1>
+  </div>
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
